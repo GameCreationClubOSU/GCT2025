fe9c157c15f44ea7fad912d4e72ff47db880877b
Lithius0 <aerosiiap@gmail.com>
2024-11-11 22:16:26 -0500

    Camera zoom

--- main_board/main_camera.gd
+++ main_board/main_camera.gd
@@ -9,12 +9,16 @@ var focus: Node2D
 ## Damping on the velocity of the camera. 
 ## Higher values means camera will slow down faster.
 @export_range(0, 100) var damping: float = 10
+## How fast the camera zooms in and out
+@export_range(0, 10) var zoom_speed: float = 1
 var velocity: Vector2 = Vector2.ZERO
 var dragging: bool = false
 ## Camera will try to keep global mouse position at this position.
 ## I.e. camera will move so that the cursor looks like it doesn't move relative
 ## to board. Set when user starts dragging.
 var drag_start: Vector2 = Vector2.ZERO
+## The value the zoom will approach over time.
+var zoom_target: Vector2 = Vector2.ONE
 
 
 # Called when the node enters the scene tree for the first time.
@@ -26,7 +30,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 		
 	if dragging:
-		var mouse_current = get_global_mouse_position()
+		var mouse_current: Vector2 = get_global_mouse_position()
 		
 		# Good old displacement / time
 		# Displacement usually current - previous, but we're moving the camera
@@ -35,8 +39,10 @@ func _process(delta: float) -> void:
 		position += (drag_start - mouse_current)
 	else:
 		position += velocity * delta
-		
 		velocity -= velocity.normalized() * velocity.length() * damping * delta
+		
+	# Zoom handling
+	zoom = zoom.move_toward(zoom_target, zoom.length() * zoom_speed * delta)
 		
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -48,7 +54,7 @@ func _unhandled_input(event: InputEvent) -> void:
 			elif event.is_released():
 				dragging = false
 		elif event.button_index == MOUSE_BUTTON_WHEEL_UP:
-			zoom *= 1.1
+			zoom_target *= 1.1
 		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-			zoom /= 1.1
+			zoom_target /= 1.1
 			
