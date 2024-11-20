d42a5a11ff201b7e8c3858fb8c14014e4041e35d
Lithius0 <aerosiiap@gmail.com>
2024-11-20 16:12:35 -0500

    Minigame manager

--- main_board/coordinator.gd
+++ main_board/coordinator.gd
@@ -15,37 +15,13 @@ var focus: Minigame:
 		
 		if is_instance_valid(focus):
 			focus.enabled = true
-
-			
-
-var minigames: Array[Minigame]
-
-## Finds all the minigames in the scene.
-func find_minigames() -> Array[Minigame]:
-	var nodes = get_tree().get_nodes_in_group("minigames")
-	var found_minigames: Array[Minigame]
-	for node in nodes:
-		if node is Minigame:
-			var minigame := node as Minigame
-			if minigame != focus:
-				minigame.enabled = false
-			found_minigames.append(minigame)
-		else:
-			push_warning("Node in minigames group is not a minigame object!")
-			node.remove_from_group("minigames")
-	
-	return found_minigames
 	
 func minigame_clicked(minigame: Minigame) -> void:
 	focus = minigame
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# Don't know if we'll ever need to resync all the minigames
-	# Perhaps some will be removed or spawn during runtime. 
-	minigames.clear()
-	for minigame in find_minigames():
-		if not minigame.clicked.is_connected(minigame_clicked):
+	
 			minigame.clicked.connect(minigame_clicked)
 
 
---
+++ minigames/minigame_manager.gd
@@ -0,0 +1,39 @@
+extends Node
+
+## Emitted when a minigame was clicked. Provides the minigame object that was clicked.
+signal minigame_clicked(minigame)
+
+var minigames: Array[Minigame] = []:
+	set(value):
+		push_error("Attempted to set value of minigames field in MinigameManager directly!")
+
+## Finds all the minigames in the scene.
+func find_minigames():
+	var nodes = get_tree().get_nodes_in_group("minigames")
+	for node in nodes:
+		if node is Minigame:
+			if node not in minigames:
+				var minigame := node as Minigame
+				# All minigames disabled on discovery.
+				# Might want to move this elsewhere, don't know if it makes
+				# the most sense for it to be here.
+				minigame.enabled = false 
+				minigame.clicked.connect(minigame_clicked.emit.bind())
+				minigames.append(minigame)
+		else:
+			# Might be worth doing some duck typing here for more flexibility.
+			# The lack of any interface in gdscript is annoying.
+			push_warning("Node in minigames group is not a minigame object!")
+			node.remove_from_group("minigames")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# May want to do this on some kind of schedule if 
+	# we want to add minigames dynamically.
+	# Or have some method here that allows minigames to register themselves.
+	find_minigames()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
