869ba50366fecfa48c076b372672e75d61853b9d
Lithius0 <aerosiiap@gmail.com>
2024-11-25 16:35:27 -0500

    Improved frame adjustment

--- minigames/minigame.gd
+++ minigames/minigame.gd
@@ -3,21 +3,28 @@ class_name Minigame
 extends SubViewportContainer
 
 signal clicked(minigame: Minigame)
+
+## This reference is used for automatically resizing the frame and not for external use.
+var _frame: NinePatchRect = get_node_or_null("Frame") as NinePatchRect
+
 @onready var viewport: SubViewport = $SubViewport
+
 var enabled: bool = false:
 	set(value):
 		enabled = value
 		viewport.process_mode = PROCESS_MODE_INHERIT if enabled else PROCESS_MODE_DISABLED
 
 func adjust_frame() -> void:
-	var frame = $Frame as NinePatchRect
-	if not is_instance_valid(frame):
-		push_error("Minigame has no frame child!")
+	if not is_instance_valid(_frame):
+		_frame = get_node_or_null("Frame") as NinePatchRect
+	if not is_instance_valid(_frame):
+		# In case the get node call is null still.
 		return
 		
-	frame.position = Vector2(-frame.patch_margin_left, -frame.patch_margin_top)
-	frame.size = size + Vector2(frame.patch_margin_left + frame.patch_margin_right,
-			frame.patch_margin_top + frame.patch_margin_bottom)
+	# Automatically adjusting frame.
+	_frame.position = Vector2(-_frame.patch_margin_left, -_frame.patch_margin_top)
+	_frame.size = size + Vector2(_frame.patch_margin_left + _frame.patch_margin_right,
+			_frame.patch_margin_top + _frame.patch_margin_bottom)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
