7b09e50300857b462e4fa1a1bd95cf11b7f9d303
Lithius0 <aerosiiap@gmail.com>
2024-11-25 14:55:04 -0500

    Automatically adjusting frame

--- minigames/minigame.gd
+++ minigames/minigame.gd
@@ -1,3 +1,4 @@
+@tool
 class_name Minigame
 extends SubViewportContainer
 
@@ -8,6 +9,16 @@ var enabled: bool = false:
 		enabled = value
 		viewport.process_mode = PROCESS_MODE_INHERIT if enabled else PROCESS_MODE_DISABLED
 
+func adjust_frame() -> void:
+	var frame = $Frame as NinePatchRect
+	if not is_instance_valid(frame):
+		push_error("Minigame has no frame child!")
+		return
+		
+	frame.position = Vector2(-frame.patch_margin_left, -frame.patch_margin_top)
+	frame.size = size + Vector2(frame.patch_margin_left + frame.patch_margin_right,
+			frame.patch_margin_top + frame.patch_margin_bottom)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_to_group("minigames")
@@ -15,9 +26,13 @@ func _ready() -> void:
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Engine.is_editor_hint():
+		adjust_frame()
 
 func _gui_input(event: InputEvent) -> void:
+	if Engine.is_editor_hint():
+		return
+		
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			clicked.emit(self)
