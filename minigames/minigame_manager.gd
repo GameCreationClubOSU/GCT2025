1036a7c0cf4c7f2cf09c911ddc070fe0cd19c24d
Lithius0 <aerosiiap@gmail.com>
2024-11-25 17:30:40 -0500

    Switched MinigameManager to use just register_minigame

--- minigames/minigame_manager.gd
+++ minigames/minigame_manager.gd
@@ -18,23 +18,8 @@ func get_root_of(node: Node) -> SubViewport:
 			
 	return null
 
-## Finds all the minigames in the scene and adds them to minigames array.
-func find_minigames():
-	var nodes: Array[Node] = get_tree().get_nodes_in_group("minigames")
-	for node in nodes:
-		if node is Minigame:
-			var minigame := node as Minigame
-			if minigame not in minigames:
-				minigame.clicked.connect(minigame_clicked.emit)
-				minigames.append(minigame)
-		else:
-			# Might be worth doing some duck typing here for more flexibility.
-			# The lack of any interface in gdscript is annoying.
-			push_warning("Node in minigames group is not a minigame object!")
-			node.remove_from_group("minigames")
-
 ## Registers a minigame to the manager.
-func register_minigame(minigame: Minigame):
+func register_minigame(minigame: Minigame) -> void:
 	if not is_instance_valid(minigame):
 		push_warning("Attempted to register invalid instance to Minigame manager!")
 		return
@@ -48,10 +33,7 @@ func register_minigame(minigame: Minigame):
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# May want to do this on some kind of schedule if 
-	# we want to add minigames dynamically.
-	# Or have some method here that allows minigames to register themselves.
-	find_minigames()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
