ec665b943d41e426862edf7801ea95f17367d749
Lithius0 <aerosiiap@gmail.com>
2024-11-30 00:58:06 -0500

    Implemented rest of ArrayInventory

--- items/array_inventory.gd
+++ items/array_inventory.gd
@@ -1,8 +1,8 @@
 class_name ArrayInventory
 extends Node
-
 ## Simple inventory. Each slot in the inventory has an associated int index.
-## TODO: Allow runtime resizing.
+## Use existing methods wherever possible. If not, the underlying array is
+## available as [member slots]. 
 
 @export var size: int = 5:
 	get:
@@ -43,6 +43,7 @@ func _ready() -> void:
 			slot.amount = descriptor.amount
 
 
+## Resizes the inventory.
 func resize(new_size: int) -> void:
 	if new_size <= 0:
 		push_error("Cannot set inventory size <= 0!")
@@ -55,3 +56,73 @@ func resize(new_size: int) -> void:
 			slots[i] = ItemSlot.new()
 	# If new_size is smaller (i.e. the inventory is shrinking) resize will
 	# remove the old slots. No further action needed.
+
+## Gets the total amount of items of type [param item_type] in the inventory.
+func amount_of(item_type: ItemType) -> int:
+	if not is_instance_valid(item_type):
+		return 0
+		
+	var sum: int = 0
+	for slot in slots:
+		if slot.item_type == item_type:
+			sum += slot.amount
+			
+	return sum
+
+## Transfers items from [param provider] to self. 
+## Will transfer a max of [param max_amount] but might transfer less depending
+## on what can be transferred.
+## Returns the amount of items transferred.
+func transfer_from(provider: ItemSlot, max_amount: int) -> int:
+	# Transferring negative items should get caught by [class ItemSlot] anyway.
+	# This is just in case.
+	max_amount = max(max_amount, 0) 
+	var amount_transferred: int = 0
+	# mini is min_integer, not synonym for small
+	var transfer_limit: int = mini(max_amount, provider.amount)
+	# Prefer slots with stackable slots first.
+	for slot in slots:
+		if transfer_limit <= 0:
+			break
+		elif slot.stackable_with(provider):
+			var transferred = slot.transfer_from(provider, transfer_limit)
+			transfer_limit -= transferred
+			amount_transferred += transferred
+			
+	# General loop
+	for slot in slots:
+		if transfer_limit <= 0:
+			break
+		var transferred = slot.transfer_from(provider, transfer_limit)
+		transfer_limit -= transferred
+		amount_transferred += transferred
+	
+	return amount_transferred
+
+## Transfers items from [param provider] to self.
+## Similar to [method transfer_from] but will try 
+## to transfer all items from [param provider] to self.
+## Returns the amount of items transferred.
+func transfer_all(provider: ItemSlot) -> int:
+	return transfer_from(provider, provider.amount)
+	
+## Attempts to find the first slot that contains items of [param type]
+## Returns negative index if not found.
+func first_slot_of(type: ItemType) -> int:
+	if not is_instance_valid(type):
+		push_error("Invalid item type!")
+		return -1
+	
+	for i in slots.size():
+		if slots[i].item_type == type:
+			return i
+	
+	return -1
+	
+## Returns true if [param slot] is in inventory. False otherwise.
+func has_slot(slot: ItemSlot) -> bool:
+	return slots.has(slot)
+
+## Gets the slot at [param index]
+func slot_at(index: int) -> ItemSlot:
+	return slots[index]
