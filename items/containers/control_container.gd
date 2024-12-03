d92a324c2f90a33f00b48a555f922e8e6be15957
Lithius0 <aerosiiap@gmail.com>
2024-12-03 14:45:04 -0500

    Simple container

---
+++ items/containers/control_container.gd
@@ -0,0 +1,25 @@
+extends Control
+## This is a simple version of a container opens when its container is clicked.
+
+@export var inventory: ArrayInventory
+@export var inventory_menu: Control
+@export var is_open: bool = false:
+	set(value):
+		is_open = value
+		inventory_menu.visible = is_open
+		
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		var mouse_event: InputEventMouseButton = event
+		if mouse_event.button_index == MOUSE_BUTTON_LEFT:
+			is_open = not is_open
