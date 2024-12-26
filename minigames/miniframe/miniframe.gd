207ab238cfbdb6363ef3d1c7e26c48d25ce3f62f
Lithius0 <aerosiiap@gmail.com>
2024-12-26 16:15:55 -0500

    Changed reload scene

--- minigames/miniframe/miniframe.gd
+++ minigames/miniframe/miniframe.gd
@@ -2,6 +2,19 @@
 class_name Miniframe
 extends SubViewportContainer
 
+## Miniframe is a portmanteau of "minigame frame".
+## It handles the visual frame and the minigame instantiated within.
+## The minigames themselves shouldn't interact with this class
+## unless the minigame wants to modify the visuals of the frame.
+## Use Coordinator, it's guaranteed to exist (for independent game testing)
+## and it should provide enough functionality.
+##
+## Something to note while reading through this class:
+## There are two roots. One is the SubViewport (local root) and the other
+## is the root of the instantiated scene (scene root). Local root is the
+## parent of scene root, don't get that confused.
+
+## Fires whenever the miniframe is clicked. It passes itself as argument.
 signal clicked(miniframe: Miniframe)
 
 ## Scale of the scene inside the viewport.
@@ -16,13 +29,30 @@ signal clicked(miniframe: Miniframe)
 			await ready
 		scene = value
 		reload_scene()
+		
+## If true, pressing the reset key will automatically reload the scene.
+## Disable if the minigame handles resets manually.
 @export var auto_reset: bool = true
+
+## Root node of the instantitated minigame scene.
+var scene_root: Node:
+	get():
+		return _scene_root
+	set(value):
+		push_error("Do not set root directly!")
 
 ## This reference is used for automatically resizing the frame and not for external use.
 var _frame_rect: NinePatchRect = get_node_or_null("Frame") as NinePatchRect
+## Reference to the root node of the instantiated scene.
+## Null if scene is not instantiated.
+var _scene_root: Node = null
 
+## Easy access to the Subviewport which is acting as the local root of the minigame.
+## This is not the scene root, this is the parent of the scene root.
 @onready var viewport: SubViewport = $SubViewport
 
+## True if the minigame is enabled. 
+## If it is not enabled, processing will be disabled. 
 var enabled: bool = false:
 	set(value):
 		enabled = value
@@ -45,15 +75,20 @@ func adjust_frame() -> void:
 func adjust_viewport() -> void:
 	viewport.size_2d_override = size * viewport_scale
 	
+## Reloads the scene in the minigame with [member scene]
+##
+## Removes the scene root node of previous scene, if it exists.
+## If there are any nodes that are a sibling of the root, they will remain. 
 func reload_scene() -> void:
-	# Clear out all child nodes in viewport before adding more
-	for child in viewport.get_children():
-		viewport.remove_child(child)
-		child.queue_free()
+	# Remove just the root node.
+	if is_instance_valid(_scene_root):
+		viewport.remove_child(_scene_root)
+		_scene_root.queue_free()
 		
 	if is_instance_valid(scene) and scene.can_instantiate():
 		var new_child: Node = scene.instantiate()
 		viewport.add_child(new_child)
+		_scene_root = new_child
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
