2b7a1ee82c65646f767e1b27ff442e5b87e002b0
Lithius0 <aerosiiap@gmail.com>
2024-12-28 17:29:15 -0500

    Improved Coordinator debug

--- coordinator/coordinator_debug.gd
+++ coordinator/coordinator_debug.gd
@@ -5,8 +5,14 @@ func render_debug() -> void:
 	if ImGui.Begin(menu_name, enabled_ref):
 		enabled = enabled_ref[0]
 		
+		ImGui.Text("In Collage: %s" % Coordinator.in_collage())
+		DebugComponents.Inventory("Collage Inventory", Coordinator.inventory)
+		
 		ImGui.SeparatorText("Registered Minigames")
-		for frame in Coordinator.miniframes:
-			ImGui.Text(DebugUtil.node_name(frame))
+		for i in len(Coordinator.miniframes):
+			var frame: Miniframe = Coordinator.miniframes[i]
+			# Just in case there's two miniframes with the same node name
+			# the ##i is there to distinguish between them.
+			DebugComponents.MiniframeNode(DebugUtil.node_name(frame) + "##" + str(i), frame)
 		
 		ImGui.End()
