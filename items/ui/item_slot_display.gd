8c77d68078927c43a22cb937a8fba69ad294aad4
Lithius0 <aerosiiap@gmail.com>
2025-01-01 15:11:01 -0500

    Translucent filtered slots

--- items/ui/item_slot_display.gd
+++ items/ui/item_slot_display.gd
@@ -36,17 +36,24 @@ func _gui_input(event: InputEvent) -> void:
 func update_display() -> void:
 	var texture: Texture2D = null
 	var amount_text: String = ""
+	var opacity = 1
 	tooltip_text = "" # tooltip_text is a field of Control.
 	
-	if is_instance_valid(slot) and not slot.is_empty():
+	if is_instance_valid(slot):
 		texture = slot.get_texture()
 		# If the max stack is only one, just show the icon and not the number.
-		amount_text = str(slot.amount) if slot.item_type.max_stack > 1 else ""
-		tooltip_text = slot.item_type.name + "\n" + slot.item_type.description
+		amount_text = str(slot.amount) if slot.amount > 1 else ""
+		if not slot.is_empty() or slot.filtered:
+			tooltip_text = slot.item_type.name + "\n" + slot.item_type.description
+		if slot.filtered and slot.is_empty():
+			# Filtered empty slots should be translucent to communicate
+			# that they are empty and to communicate the filter.
+			opacity = 0.5 
 		
 	# If the control nodes aren't set, assume that the user wants to not show them. 
 	if is_instance_valid(_item_texture):
 		_item_texture.texture = texture
+		_item_texture.self_modulate = Color(1, 1, 1, opacity)
 		
 	if is_instance_valid(_amount_label):
 		_amount_label.text = amount_text
