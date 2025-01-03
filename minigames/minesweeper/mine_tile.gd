88d082afa89c18540fc2cd2a5fa66282cca0f31f
Lithius0 <aerosiiap@gmail.com>
2025-01-03 00:25:22 -0500

    Colors

--- minigames/minesweeper/mine_tile.gd
+++ minigames/minesweeper/mine_tile.gd
@@ -3,20 +3,34 @@ class_name MineTile
 
 signal revealed
 
+## These are close-ish to the orginal minesweeper colors
+## Many of the colors were changed for contrast reasons
+static var colors: Array[Color] = [
+	Color.WHITE, # 0 doesn't have a color, but we still need a value here
+	Color.DODGER_BLUE,
+	Color.LIME_GREEN,
+	Color.ORANGE_RED,
+	Color.CORNFLOWER_BLUE,
+	Color.CRIMSON,
+	Color.CYAN,
+	Color.GOLD,
+	Color.PURPLE,
+]
+
 @export var cover: Control
 @export var mine: Control
 @export var number: Label
 @export var flag: Control
 
-@export var adjacent_mines: int = 0
-@export var is_mine: bool = false
-
+var adjacent_mines: int = 0
+var is_mine: bool = false
 var flagged: bool = false:
 	set(value):
 		if not is_revealed:
 			flagged = value
 			flag.visible = value
 var is_revealed: bool = false
+var label_settings = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -48,6 +62,8 @@ func reveal() -> void:
 	if is_mine:
 		mine.visible = true
 	elif adjacent_mines > 0:
+		number.label_settings = number.label_settings.duplicate()
+		number.label_settings.font_color = colors[adjacent_mines]
 		number.text = str(adjacent_mines)
 		number.visible = true
 	
